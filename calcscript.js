db03fe018e0d269dfaf98f9fbdf5fbfc4209092b
Shea Cronin <shealcronin@gmail.com>
2020-12-05 20:57:20 -0500

    Add more functions

--- calcscript.js
+++ calcscript.js
@@ -28,16 +28,29 @@ const operators = document.querySelectorAll('.operator');
 const screen = document.querySelector('#screen');
 const equals = document.querySelector('#equals');
 
+let a;
+let b;
+let operator;
+
 function displayNum(e) {
     const clickedNum = e.target.textContent;
     screen.textContent += clickedNum;
 }
 
 function displayOperator(e) {
-    
+    const clickedOperator = e.target.textContent;
+    screen.textContent += clickedOperator;
+
+    a = screen.textContent;
+    operator = clickedOperator;
+}
+
+function getB() {
+    // screen.textContent.splice(indexOf)
 }
 
 nums.forEach(num => num.addEventListener('click', displayNum));
 operators.forEach(operator => operator.addEventListener('click', displayOperator));
 
+equals.addEventListener('click', getB);
 equals.addEventListener('click', () => operate(add, 1, 2));
