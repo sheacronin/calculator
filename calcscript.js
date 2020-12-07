e21948704af3e13f6fd101d13de2ccbc6caa30a4
Shea Cronin <shealcronin@gmail.com>
2020-12-07 09:52:57 -0500

    Add operate functionality

--- calcscript.js
+++ calcscript.js
@@ -14,43 +14,76 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, a, b) {
-    console.log(a);
-    return operator(a, b);
-}
-
-function sum(array) {
-	return array.reduce(((sum, n) => sum + n), 0);
-}
-
 const nums = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
 const screen = document.querySelector('#screen');
 const equals = document.querySelector('#equals');
+const clear = document.querySelector('#clear');
 
-let a;
-let b;
+let a = '';
+let b = '';
 let operator;
+let displayingOperator = false; // Boolean to tell if nums should be part of a or b.
+let displayingResult = false; // Booleon to check if number is result of operation.
 
 function displayNum(e) {
     const clickedNum = e.target.textContent;
-    screen.textContent += clickedNum;
+    if (displayingOperator) b += clickedNum;
+    
+    if (displayingResult) {
+        screen.textContent = clickedNum;
+        displayingResult = false;
+    } else {
+        screen.textContent += clickedNum;
+    }
+    console.log(`display num. A: ${a}, Operator: ${operator}, B: ${b}`);
 }
 
 function displayOperator(e) {
+    if (displayingOperator) { // Check if there are already 2 nums being operated.
+
+    }
+
+    a = screen.textContent;
+
     const clickedOperator = e.target.textContent;
     screen.textContent += clickedOperator;
 
-    a = screen.textContent;
-    operator = clickedOperator;
+    operator = e.target.id;
+    displayingOperator = true;
+    console.log(`display operator. A: ${a}, Operator: ${operator}, B: ${b}`);
 }
 
-function getB() {
-    // screen.textContent.splice(indexOf)
+function operate() {
+    a = parseInt(a);
+    b = parseInt(b);
+
+    console.log(a + ' ' + operator + ' ' + b);
+
+    displayingOperator = false;
+
+    screen.textContent = window[operator](a, b);
+    displayingResult = true;
+
+    a = '';
+    b = '';
+    displayingOperator = false;
+
+    console.log(`operate. A: ${a}, Operator: ${operator}, B: ${b}`);
+}
+
+function clearCalc(e) {
+    screen.textContent = '';
+    a = '';
+    b = '';
+    displayingOperator = false;
+
+    console.log(`clear Calc. A: ${a}, Operator: ${operator}, B: ${b}`)
 }
 
 nums.forEach(num => num.addEventListener('click', displayNum));
 operators.forEach(operator => operator.addEventListener('click', displayOperator));
 
-equals.addEventListener('click', getB);
-equals.addEventListener('click', () => operate(add, 1, 2));
+equals.addEventListener('click', operate);
+
+clear.addEventListener('click', clearCalc);
